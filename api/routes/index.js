b828c652ce8bd283b58e36e5f72545bc664df009
Zenidd <jpsaezgutierrez@gmail.com>
2023-08-18 15:46:31 +0200

    api-issue-16: add basic auth to the API

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const authMiddleware = require('./auth-middleware');
+router.use(authMiddleware);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {  
